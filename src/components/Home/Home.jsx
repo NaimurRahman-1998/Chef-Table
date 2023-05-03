1e371bd2d26d8af4ab377b818e804b5bb549378f
NaimurRahman-1998 <nrturzo1998111@gmail.com>
2023-05-03 16:26:13 +0600

    editing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -29,22 +29,30 @@ const Home = () => {
                     <SwiperSlide> <img src="https://www.foodandwine.com/thmb/jJupeS5vHMkh9TBsJkwdaG1uPY8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Tamarind-Chicken-FT-RECIPE0522-80072d93f7bc4bc7abf1dcf5b5317b0c.jpg" alt="" /> </SwiperSlide>
                     <SwiperSlide> <img src="https://static.onecms.io/wp-content/uploads/sites/43/2022/04/06/233613-Best-Damn-Chili-mfs-242.jpg" alt="" /> </SwiperSlide>
                     <SwiperSlide> <img src="https://www.forksoverknives.com/wp-content/uploads/spaghetti-meatballs-wordpress-1.jpg" alt="" /> </SwiperSlide>
-                    
+
                 </Swiper>
             </div>
 
+            <h1 className='text-3xl underline text-center font-bold mt-5' >Our Chefs</h1>
+
             <div id='cards' className='mt-5  grid grid-cols-3 gap-12'>
                 {
-                    data.map((chef) => 
-                        <div key={chef.id} className='border-2 rounded p-5'>
+                    data.map((chef) =>
+                        <div key={chef.id} className=' rounded p-5'>
                             <img className='h-[15rem] w-[20] rounded-full mx-auto' src={chef.image} alt="" />
                             <h1 className='text-center text-xl font-semibold mt-2'>{chef.name}</h1>
-                            <p className='mt-3 mb-3'>Experience : {chef.experience}</p>
-                            <p className='mb-4'> Number OF Recipes : {chef.recipes.length}</p>
-                            <div className='flex justify-end items-center gap-2'>
-                                <BsSuitHeart />
-                                {chef.likes}
-                                <Link to={`/chefsInfo/${chef.id}`}><button className='btn rounded-full'>View Recipes</button></Link>
+                            <div className='flex justify-between'>
+                                <div>
+                                    <p className='mt-3 mb-3'>Experience : {chef.experience}</p>
+                                    <p className='mb-4'> Number OF Recipes : {chef.recipes.length}</p>
+                                </div>
+                                <div className='flex  items-center gap-2'>
+                                    <div className='flex'>
+                                        <BsSuitHeart className='text-red-500 text-2xl' />
+                                        {chef.likes}
+                                    </div>
+                                    <Link to={`/chefsInfo/${chef.id}`}><button className='btn rounded-full'>View Recipes</button></Link>
+                                </div>
                             </div>
                         </div>
 
